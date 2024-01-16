ed095b09ff52e339c0686a2d354c7173bc05cafb
Artem Leonov <artemleonov@yahoo.com>
2024-01-16 07:53:36 +0300

    Повторный коммит

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButon: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
     @IBOutlet weak var buttonTouch: UIButton!
     @IBOutlet weak var textCount: UILabel!
     var countNumber: Int = 0
@@ -28,5 +31,17 @@ class ViewController: UIViewController {
         textCount.text = "Значение счётчика: \(countNumber)"
     }
     
+    @IBAction func deletAll(_ sender: Any) {
+        countNumber = 0
+        updateTrrr()
+    }
+    @IBAction func addNumber(_ sender: Any) {
+        countNumber += 1
+        updateTrrr()
+    }
+    @IBAction func minusNumber(_ sender: Any) {
+        countNumber = max(0, countNumber - 1)
+        updateTrrr()
+    }
 }
 
