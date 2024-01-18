b0631b465a78ac2528709d4139432ad8642bab8b
Artem Leonov <artemleonov@yahoo.com>
2024-01-18 07:13:39 +0300

    Проверил приложение, Разобрался со всеми ошибками.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -12,22 +12,16 @@ class ViewController: UIViewController {
     @IBOutlet var minusButton: UIButton!
     @IBOutlet var plusButon: UIButton!
     @IBOutlet var resetButton: UIButton!
-    @IBOutlet var buttonTouch: UIButton!
     @IBOutlet var textCount: UILabel!
     var countNumber: Int = 0
     var history: String = "История изменений:\n"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateTrrr()
+        counterUpdater()
         historyText.text = history
 
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func updateButton(_ sender: Any) {
-        countNumber += 1
-        updateTrrr()
     }
 
     func updateHistory(with message: String) {
@@ -38,26 +32,26 @@ class ViewController: UIViewController {
         historyText.text = history
     }
 
-    func updateTrrr() {
+    func counterUpdater() {
         textCount.text = "Значение счётчика: \(countNumber)"
     }
 
     @IBAction func deletAll(_ sender: Any) {
         countNumber = 0
-        updateTrrr()
+        counterUpdater()
         updateHistory(with: "значение сброшено")
     }
 
     @IBAction func addNumber(_ sender: Any) {
         countNumber += 1
-        updateTrrr()
+        counterUpdater()
         updateHistory(with: "значение изменено на +1")
     }
 
     @IBAction func minusNumber(_ sender: Any) {
         if countNumber > 0 {
             countNumber -= 1
-            updateTrrr()
+            counterUpdater()
             updateHistory(with: "значение изменено на -1")
         } else {
             // Здесь countNumber уже равен 0, и пользователь пытается уменьшить его еще больше
